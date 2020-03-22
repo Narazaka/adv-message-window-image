84b42fbc89c9cc354a7b2f03a8c7297dbfc0bf8f
Narazaka <info@narazaka.net>
2020-03-22 22:29:08 +0900

    firebase config separate

---
+++ frontend/firebaseConfig.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8aQJvtlEeWd7vBh8EUU4nQJiji7WwDs0",
+    authDomain: "adv-message-window-image.firebaseapp.com",
+    databaseURL: "https://adv-message-window-image.firebaseio.com",
+    projectId: "adv-message-window-image",
+    storageBucket: "adv-message-window-image.appspot.com",
+    messagingSenderId: "354320701187",
+    appId: "1:354320701187:web:fe7226a0a25299bc48fb36",
+    measurementId: "G-V7FFRFLSV6",
+};
+
+export { firebaseConfig };
